456003a0b4fb1c0ecc1b3f52fe47ef7a986e07fb
iamkarthikram <59197955+iamKarthikRam@users.noreply.github.com>
2023-12-27 07:49:54 +0000

    Polymorphic component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,27 @@
-import { type ElementType, FC, ReactNode } from "react";
+import {
+  type ElementType,
+  // FC,
+  ReactNode,
+  ComponentPropsWithoutRef,
+} from "react";
 
-type ContainerProps = {
-  as: ElementType;
+type ContainerProps<T extends ElementType> = {
+  as?: T;
   children: ReactNode;
-};
+} & ComponentPropsWithoutRef<T>;
 
-const Container: FC<ContainerProps> = ({ as, children }: ContainerProps) => {
-  const Component = as;
-  return <Component>{children} </Component>;
-};
+// const Container<C extends ElementType>: FC = ({ as, children }: ContainerProps<C>) => {
+//   const Component = as;
+//   return <Component>{children} </Component>;
+// };
 
-export default Container;
+// export default Container;
+
+export default function Container<C extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  const Component = as || "div";
+  return <Component {...props}>{children} </Component>;
+}
